d60a7fbe0b305f3582563e8d437cef4f9c3aa89a
Jesse Neumann <jesseneumann@gmail.com>
2016-12-12 22:18:24 -0700

    added some fields to state

--- src/state.js
+++ src/state.js
@@ -9,6 +9,10 @@
   ],
   cash: 2000,
   debt: 5500,
+  inventory: {
+    current: 0,
+    max: 100
+  },
   bank: 0,
   day: 0,
   totalDays: 30,
@@ -17,7 +21,62 @@
       qty: 0,
       price: 0,
       special: 'normal'
-    } //more drugs will go here...
+    },
+    weed: {
+      qty: 0,
+      price: 0,
+      special: 'normal'
+    },
+    ludes: {
+      qty: 0,
+      price: 0,
+      special: 'normal'
+    },
+    opium: {
+      qty: 0,
+      price: 0,
+      special: 'normal'
+    },
+    pcp: {
+      qty: 0,
+      price: 0,
+      special: 'normal'
+    },
+    peyote: {
+      qty: 0,
+      price: 0,
+      special: 'normal'
+    },
+    shrooms: {
+      qty: 0,
+      price: 0,
+      special: 'normal'
+    },
+    speed: {
+      qty: 0,
+      price: 0,
+      special: 'normal'
+    },
+    acid: {
+      qty: 0,
+      price: 0,
+      special: 'normal'
+    },
+    cocain: {
+      qty: 0,
+      price: 0,
+      special: 'normal'
+    },
+    heroin: {
+      qty: 0,
+      price: 0,
+      special: 'normal'
+    },
+    mda: {
+      qty: 0,
+      price: 0,
+      special: 'normal'
+    } //more drugs can go here...
   }
 }
 
